a2f22f41aa7fc49a4767245f89bfc4fbdac6f583
hoant <tranvanhoan@shippo.vn>
2018-05-10 17:34:29 +0700

    sửa lại test case

--- test/i18n.singlePhrase.js
+++ test/i18n.singlePhrase.js
@@ -1,4 +1,4 @@
-var i18n = require('../i18n').I18n,
+var I18n = require('../i18n').I18n,
     should = require("should"),
     fs = require('fs');
 
@@ -10,43 +10,40 @@ function putJson(l, d) {
 
 describe('Module Defaults', function() {
 
-    var TestScope = {};
+    var translator = {};
     var locales = ['en', 'de', 'fr'];
 
-    beforeEach(function() {
-        TestScope = {};
-        TestScope = i18n.init({
-            locales: locales,
-            directory: "./locales",
-            updateFiles: true,
-            syncFiles: true,
+    before(function() {
+        I18n.init({
+            locales: ['en', 'de', 'fr', 'ru'],
+            directory: './locales',
+            register: translator,
+            updateFiles: false,
             objectNotation: true
         });
-
-        TestScope.setLocale('en');
     });
 
     it('__() should return translated parsed and substituted string in en', function(done) {
-        TestScope.setLocale('en');
-        should.deepEqual(TestScope.__('Hello'), 'Hello');
-        should.deepEqual(TestScope.__('Hello %s', 'Marcus'), 'Hello Marcus');
-        should.deepEqual(TestScope.__('Hello {{name}}', { name: 'Marcus' }), 'Hello Marcus');
+        translator.switchLocale('en');
+        should.deepEqual(translator.__('Hello'), 'Hello');
+        should.deepEqual(translator.__('Hello %s', 'Marcus'), 'Hello Marcus');
+        should.deepEqual(translator.__('Hello {{name}}', { name: 'Marcus' }), 'Hello Marcus');
 
         done();
     });
     it('__() should return translated parsed and substituted string in de', function(done) {
-        TestScope.setLocale('de');
-        should.deepEqual(TestScope.__('Hello'), 'Hallo');
-        should.deepEqual(TestScope.__('Hello %s', 'Marcus'), 'Hallo Marcus');
-        should.deepEqual(TestScope.__('Hello {{name}}', { name: 'Marcus' }), 'Hallo Marcus');
+        translator.switchLocale('de');
+        should.deepEqual(translator.__('Hello'), 'Hallo');
+        should.deepEqual(translator.__('Hello %s', 'Marcus'), 'Hallo Marcus');
+        should.deepEqual(translator.__('Hello {{name}}', { name: 'Marcus' }), 'Hallo Marcus');
 
         done();
     });
     it('__() should return translated parsed and substituted string in fr', function(done) {
-        TestScope.setLocale('fr');
-        should.deepEqual(TestScope.__('Hello'), 'Bonjour');
-        should.deepEqual(TestScope.__('Hello %s', 'Marcus'), 'Bonjour Marcus');
-        should.deepEqual(TestScope.__('Hello {{name}}', { name: 'Marcus' }), 'Bonjour Marcus');
+        translator.switchLocale('fr');
+        should.deepEqual(translator.__('Hello'), 'Bonjour');
+        should.deepEqual(translator.__('Hello %s', 'Marcus'), 'Bonjour Marcus');
+        should.deepEqual(translator.__('Hello {{name}}', { name: 'Marcus' }), 'Bonjour Marcus');
 
         done();
     });
